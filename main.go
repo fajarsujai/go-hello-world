aaa2d7e94a958021e13566197e2ccdfd20c42a9a
fajarsujai <fajar.s.fratama@gmail.com>
2023-04-18 11:17:41 +0700

    add env

--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
-	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+	// godotenv package
+	dotenv := goDotEnvVariable("app")
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello "+dotenv)
 	})
-	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/hello\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
