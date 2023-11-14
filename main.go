2dd854106ca213a48c3e81c2837a88c1a82d7df2
fajarsujai <fajar.s.fratama@gmail.com>
2023-11-14 21:08:15 +0700

    update 1

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello WORLD")
+		fmt.Fprintf(w, "Hello WORLD (UPDATE 1)")
 	})
 	fmt.Printf("Server running (port=8000), route: http://localhost:8000\n")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
