e7ba0f774f78ddd994859a7e86be4ff281c48932
fajarsujai <fajar.s.fratama@gmail.com>
2024-02-20 18:55:18 +0700

    update dockerfile

--- main.go
+++ main.go
@@ -6,25 +6,34 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	// godotenv package
 	appenv := os.Getenv("APPENV")
 	apport := os.Getenv("PORT")
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
+	name := os.Getenv("username")
+	age := os.Getenv("password")
+	
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Update 1 dan Hello "+appenv)
 	})
 
-	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Nama "+name)
-		fmt.Fprintf(w, "Nama "+age)
+		fmt.Fprintf(w, "Age "+age)
+
+	})
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Username "+username)
+		fmt.Fprintf(w, "Password "+password)
 
 	})
 
